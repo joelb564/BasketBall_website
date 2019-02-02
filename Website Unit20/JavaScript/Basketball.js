906daa941b60f94ee611c2d09e34e6de65c71488
joelb564 <joelbutler@joels-air.home>
2019-02-02 15:36:34 +0000

    Javascript Updated

---
+++ Website Unit20/JavaScript/Basketball.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = +1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function check(form)
+        {
+          if(form.username.value == "Tory" && form.Password.value == "Unit20")
+            {
+              window.open('HomePage.html')
+            }
+        else
+        {
+          alert("The username and password you entered don't match")
+        }
+        }
